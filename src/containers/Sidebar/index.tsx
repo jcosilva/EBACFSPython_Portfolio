ee0946ed3099cbff9a0058c90c7ddb9ec69bb36b
Julio Silva <silvajulio@hotmail.com>
2024-12-04 17:48:16 -0500

    Estilização final do Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/jcosilva.png" />
-    <Titulo fontSize={20}>Julio Silva</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Julio Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        jcosilva
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante Front-End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
