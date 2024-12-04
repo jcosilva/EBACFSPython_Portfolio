a7187017b32405ac4eb9d8f7446bdba3314c2b23
Julio Silva <silvajulio@hotmail.com>
2024-12-04 18:18:53 -0500

    Versão final do site

---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+const temaDark = {
+  corPrincipal: '#ffffff',
+  corSecundaria: '#eeeeee',
+  corDeFundo: '#282a35',
+  corDeFundoBotao: '#ffffff',
+  codDeBorda: '#c1c1c1'
+}
+
+export default temaDark
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  codDeBorda: string
+}
